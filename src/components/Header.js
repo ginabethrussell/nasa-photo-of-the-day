95aee9eacc6d60b543d5fb5d7863d2956deba690
Gina Beth Russell <ginabethrussell@Ginas-MBP.cinci.rr.com>
2020-10-20 13:47:27 -0400

    style changes added with styled components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const HeaderDiv = styled.div`
+    background-color: black;
+    color: white;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    height: 100px;
+    width: 100%;
+    font-size: 2rem;
+`;
+
+const HeaderImg = styled.img`
+    height: 75px`;
 
 export default function Header() {
     // Static component
     // Accepts no props, sets no component variables
     return (
-        <div className="header">
-            <img src="https://www.nasa.gov/sites/all/themes/custom/nasatwo/images/nasa-logo.svg" alt="nasa logo" />
+        <HeaderDiv>
+            <HeaderImg src="https://www.nasa.gov/sites/all/themes/custom/nasatwo/images/nasa-logo.svg" alt="nasa logo" />
             <h1> NASA Astronomy Picture of the Day</h1>
-        </div>
+        </HeaderDiv>
     )
 }
--- src/components/Media.js
+++ src/components/Media.js
@@ -1,23 +1,38 @@
 import React from 'react';
+import styled from "styled-components";
 
+const ImageDiv = styled.div`
+    width: 45%;
+    max-width: 400px;
+    margin: 0 auto;
+    @media(max-width: 800px){
+        width: 100%;
+        max-width: 600px;
+    }
+`;
 
+ const ImageDivImg = styled.img`
+    margin: 0 auto;
+    width: 100%;
+    max-width: 600px;
+ `;
 export default function Media(props) {
     const {mediaType, title, src, copyright} = props;
   // Receives data from Main as props
     return (
-        <div className='image'> 
+        <ImageDiv> 
             <h2>{title}</h2>
 
             {/* Create conditional render for image or video */}
             {
                 mediaType === 'image'? 
-                    <img src={src} max-width="100%"/> : 
+                    <ImageDivImg src={src} max-width="100%"/> : 
                     <iframe width="420" height="315"
                     src={src + "?autoplay=1&mute=1"}>
                     </iframe>      
             }
             {/*Create conditional render if data contains copyright value - not null */}
            <p>{copyright && `Copyright: ${copyright}`}</p>
-        </div>
+        </ImageDiv>
     )
 }
